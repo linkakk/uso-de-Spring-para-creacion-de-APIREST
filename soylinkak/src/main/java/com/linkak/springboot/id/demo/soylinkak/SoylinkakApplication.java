07015e7b7a581567deed67ca9078ed8e96855fa9
linkakk <b.a.j.d@hotmail.com>
2024-08-21 18:48:59 -0500

    el comiezo
    inicio

---
+++ soylinkak/src/main/java/com/linkak/springboot/id/demo/soylinkak/SoylinkakApplication.java
@@ -0,0 +1,13 @@
+package com.linkak.springboot.id.demo.soylinkak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoylinkakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoylinkakApplication.class, args);
+	}
+
+}
---
+++ soylinkak/src/test/java/com/linkak/springboot/id/demo/soylinkak/SoylinkakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkak.springboot.id.demo.soylinkak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoylinkakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
